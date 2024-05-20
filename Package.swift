09e92d6ab7e61fc71f232e15a57e4f058de31192
Kalpesh Jetani <kalpesh.jetani@smartsensesolutions.com>
2024-05-20 10:57:43 +0530

    Squashed commit of the following:
    commit ce2786871227a09be4b5637cbcf48eb5b3aaa50a
    Author: Kalpesh Jetani <kalpesh.jetani@smartsensesolutions.com>
    Date:   Mon May 20 10:54:55 2024 +0530
    
        minor -  system generated config
    
    commit 7e6d4660c5a2380ef852ee8f30ef93b600fb2499
    Author: Kalpesh Jetani <kalpesh.jetani@smartsensesolutions.com>
    Date:   Fri May 17 19:14:44 2024 +0530
    
        path
    
    commit 9b722a34640714fb59848deab0dfcddd3f50fe18
    Author: Kalpesh Jetani <kalpesh.jetani@smartsensesolutions.com>
    Date:   Fri May 17 19:13:22 2024 +0530
    
        path
    
    commit 2457e732229444c9920af45d6293ccaff092be6d
    Author: Kalpesh Jetani <kalpesh.jetani@smartsensesolutions.com>
    Date:   Fri May 17 19:12:19 2024 +0530
    
        exclude path
    
    commit 8e5304ce17d0aae3ae739d7bde1d36219e3fc030
    Author: Kalpesh Jetani <kalpesh.jetani@smartsensesolutions.com>
    Date:   Fri May 17 19:08:34 2024 +0530
    
        package management
    
    commit 13cff7099ab3395772177eba5e517076dbad92d0
    Author: Kalpesh Jetani <kalpesh.jetani@smartsensesolutions.com>
    Date:   Fri May 17 18:59:57 2024 +0530
    
        Package source moved out
    
    commit 3747bb2d25efad256e25aa839dba9a36d2014799
    Author: Kalpesh Jetani <kalpesh.jetani@smartsensesolutions.com>
    Date:   Fri May 17 18:56:04 2024 +0530
    
        path
    
    commit 458ada2021444ab1b119599387ba5927b1bbbfbc
    Author: Kalpesh Jetani <kalpesh.jetani@smartsensesolutions.com>
    Date:   Fri May 17 18:53:16 2024 +0530
    
        path
    
    commit 25cdd1daf92a072b00d813d856346fcaba0adf5e
    Author: Kalpesh Jetani <kalpesh.jetani@smartsensesolutions.com>
    Date:   Fri May 17 18:52:15 2024 +0530
    
        path
    
    commit 821cd7cb6e7d4e09f0a26f34c8e8f21244828434
    Author: Kalpesh Jetani <kalpesh.jetani@smartsensesolutions.com>
    Date:   Fri May 17 18:51:32 2024 +0530
    
        path
    
    commit 82253f56b76f3cc9ccf66b62d67fe1d81aa59e5d
    Author: Kalpesh Jetani <kalpesh.jetani@smartsensesolutions.com>
    Date:   Fri May 17 18:50:41 2024 +0530
    
        path
    
    commit 52362fae2bfc8e1d84e784d10b5468e6d4de742a
    Author: Kalpesh Jetani <kalpesh.jetani@smartsensesolutions.com>
    Date:   Fri May 17 18:49:13 2024 +0530
    
        package.swift
    
    commit 44827a64b1d5de6ba43076ebd1b1fda14ec774c8
    Author: Kalpesh Jetani <kalpesh.jetani@smartsensesolutions.com>
    Date:   Fri May 17 18:36:07 2024 +0530
    
        last revision
    
    commit 6c3cde9faaa9ed9ee26df09741e243fbbf2fd66f
    Author: Kalpesh Jetani <kalpesh.jetani@smartsensesolutions.com>
    Date:   Fri May 17 18:35:09 2024 +0530
    
        path update exclude ios_example
    
    commit 19a83dcbea6b78850ced852ef42ec65b520fb2ef
    Author: Kalpesh Jetani <kalpesh.jetani@smartsensesolutions.com>
    Date:   Fri May 17 18:31:57 2024 +0530
    
        path update exclude ios_example
    
    commit 95109ad0163f9c765060a6d28cf9a57488275456
    Author: Kalpesh Jetani <kalpesh.jetani@smartsensesolutions.com>
    Date:   Fri May 17 18:28:32 2024 +0530
    
        Revert "path test updates"
    
        This reverts commit db13bc9b6ceeb6b34e15d9dbab6c1e2fd1145d0f.
    
    commit db13bc9b6ceeb6b34e15d9dbab6c1e2fd1145d0f
    Author: Kalpesh Jetani <kalpesh.jetani@smartsensesolutions.com>
    Date:   Fri May 17 18:27:57 2024 +0530
    
        path test updates
    
    commit 7530b9cf1dc3770f0f3ddd7df43ff13cc2e8f781
    Author: Kalpesh Jetani <kalpesh.jetani@smartsensesolutions.com>
    Date:   Fri May 17 18:12:03 2024 +0530
    
        test path update
    
    commit fcfa4b9122770c60de98f767d8251c08029e93f5
    Author: Kalpesh Jetani <kalpesh.jetani@smartsensesolutions.com>
    Date:   Fri May 17 17:32:20 2024 +0530
    
        path revision

---
+++ Package.swift
@@ -0,0 +1,40 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SSLazyList",
+    platforms: [.iOS(.v15)],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "SSLazyList",
+            targets: ["SSLazyList"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "SSLazyList", exclude: ["iOS_Example",
+"iOS_Example/iOS_Example",
+"iOS_Example/iOS_Example/Models",
+"iOS_Example/iOS_Example/Models/UserModel.swift",
+"iOS_Example/iOS_Example/Preview Content",
+"iOS_Example/iOS_Example/Preview Content/Preview Assets.xcassets",
+"iOS_Example/iOS_Example/Services",
+"iOS_Example/iOS_Example/Services/UserDataService.swift",
+"iOS_Example/iOS_Example/Assets.xcassets",
+"iOS_Example/iOS_Example/ContentView.swift",
+"iOS_Example/iOS_Example/iOS_Example.entitlements",
+"iOS_Example/iOS_Example/iOS_ExampleApp.swift",
+"iOS_Example/iOS_ExampleTests",
+"iOS_Example/iOS_ExampleTests/iOS_ExampleTests.swift",
+"iOS_Example/iOS_ExampleUITests",
+"iOS_Example/iOS_ExampleUITests/iOS_ExampleUITests.swift",
+"iOS_Example/iOS_ExampleUITests/iOS_ExampleUITestsLaunchTests.swift"]),
+        .testTarget(
+            name: "SSLazyListTests",
+            dependencies: ["SSLazyList"]),
+    ]
+)
