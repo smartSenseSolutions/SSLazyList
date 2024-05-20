c17a7e2b007fed639b5432974bb9537b25693ab7
Kalpesh Jetani <kalpesh.jetani@smartsensesolutions.com>
2024-05-20 11:19:25 +0530

    exclude paths of directory removed

--- Package.swift
+++ Package.swift
@@ -16,20 +16,15 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "SSLazyList", exclude: ["iOS_Example",
-                    "iOS_Example/Models",
+            name: "SSLazyList", exclude: [
                     "iOS_Example/Models/UserModel.swift",
-                    "iOS_Example/Preview Content",
                     "iOS_Example/Preview Content/Preview Assets.xcassets",
-                    "iOS_Example/Services",
                     "iOS_Example/Services/UserDataService.swift",
                     "iOS_Example/Assets.xcassets",
                     "iOS_Example/ContentView.swift",
                     "iOS_Example/iOS_Example.entitlements",
                     "iOS_Example/iOS_ExampleApp.swift",
-                    "iOS_ExampleTests",
                     "iOS_ExampleTests/iOS_ExampleTests.swift",
-                    "iOS_ExampleUITests",
                     "iOS_ExampleUITests/iOS_ExampleUITests.swift",
                     "iOS_ExampleUITests/iOS_ExampleUITestsLaunchTests.swift"]),
 
